fa9d0bf4ca99b7fb7abc30b03b15eae19e16386d
William Carolin <wcarolin-contractor@zappos.com>
2017-07-03 14:00:44 -0700

    Original File load.

---
+++ SquashyToad/Assets/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadNextScene(){
+		int currentIndex = SceneManager.GetActiveScene ().buildIndex;
+		SceneManager.LoadScene (currentIndex + 1);
+	}
+
+	public void Quit(){
+		Debug.Log ("Application will Quit");
+		Application.Quit ();
+	}
+}
