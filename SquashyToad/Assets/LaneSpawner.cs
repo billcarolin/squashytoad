9c583e60137691ac3352cf7e6c1e1df77de4554e
William Carolin <wcarolin-contractor@zappos.com>
2017-07-06 22:27:29 -0700

    Created Lane Spawners.

---
+++ SquashyToad/Assets/LaneSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaneSpawner : MonoBehaviour {
+
+	public GameObject[] lanePrefabs;
+
+
+	// Use this for initialization
+	void Start () {
+		CreateRandomLane (0);
+		CreateRandomLane (10);
+		CreateRandomLane (20);
+		CreateRandomLane (30);
+		CreateRandomLane (40);
+		
+	}
+
+	void CreateRandomLane(float offset){
+		int laneIndex = Random.Range (0, lanePrefabs.Length);
+		var lane = Instantiate (lanePrefabs [laneIndex]);
+		lane.transform.parent = transform;
+		lane.transform.Translate (0, 0, offset);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ SquashyToad/Assets/TreeSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSpawner : MonoBehaviour {
+
+	public GameObject treePrefab;
+	public int maximumTrees = 10;
+
+
+	// Use this for initialization
+	void Start () {
+		int totalTrees = Random.Range (2, maximumTrees);
+		createRandomTrees (totalTrees);
+	}
+
+	void createRandomTrees (int treeCount) {
+		for (int x = 0; x < treeCount; x++) {
+			CreateRandomTree ();
+		}
+	}
+
+	void CreateRandomTree(){
+		float treeX = Random.Range (-50, 50);
+		float treeZ = Random.Range (-5, 5);
+		var tree = Instantiate (treePrefab);
+		tree.transform.parent = transform;
+		tree.transform.localPosition = new Vector3(treeX, 0, treeZ);
+		//tree.transform.localScale = new Vector3(0.25f,0.25f,0.25f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
