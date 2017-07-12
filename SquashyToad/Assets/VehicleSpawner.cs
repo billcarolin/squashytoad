f428281da9c19ff3e5c323a54ad89941bfbf1cfd
William Carolin <wcarolin-contractor@zappos.com>
2017-07-11 19:37:01 -0700

    Added Vehicle spawning.

---
+++ SquashyToad/Assets/VehicleSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehicleSpawner : MonoBehaviour {
+
+	public GameObject prefab;
+
+	public float minTime = 2;
+	public float meanTime = 4;
+	float nextSpawnTime = 0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > nextSpawnTime)
+		{
+			Spawn();
+			nextSpawnTime = Time.time + minTime -Mathf.Log (Random.value) * meanTime;
+		}
+	}
+
+	void Spawn()
+	{
+		var instance = Instantiate(prefab, transform.position, transform.rotation, transform);
+	}
+}
